d8e5af2bb37526dd0c3e985c9ee020978fb9f07f
abdul wahab <abdul.wahab@codeforesight.com>
2023-08-17 15:02:24 +0500

    Added: add navbar in components directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
-          <Route></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/cart' element={<Cart />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
